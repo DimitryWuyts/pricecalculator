c61f0a6302306d974fda8badacdc3aeed2877baa
dimitry <dimiw1337@hotmail.com>
2020-02-17 04:30:14 -0500

    blank

--- controller/HomepageController.php
+++ controller/HomepageController.php
@@ -14,6 +14,6 @@ class HomepageController
         // then the view will actually display them.
 
         //load the view
-        require 'View/homepage.php';
+        require 'view/homepage.php';
     }
 }
--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <?php
 declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+//include header
+require  'view/includes/header.php';
+
+
+
+
 
 //include all your model files here
 require 'model/User.php';
 //include all your controllers here
 require 'controller/HomepageController.php';
 
+
+
+
+
+
+//include footer
+require  'view/includes/footer.php';
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
-$controller = new HomepageController();
-$controller->render($_GET, $_POST);
+
+//$controller = new HomepageController();
+//
+//$controller->render($_GET, $_POST);
