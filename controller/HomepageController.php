96a28d1b192b78a84b61309c2715cfdf4e72676d
moeti <moetichentuf@hotmail.com>
2020-02-17 11:18:33 +0100

    trying to fetch

--- controller/HomepageController.php
+++ controller/HomepageController.php
@@ -8,8 +8,17 @@ class HomepageController
     public function render(array $GET, array $POST)
     {
         //this is just example code, you can remove the line below
-        $user = new User('John Smith');
+        // Takes raw data from the request
+        $json = file_get_contents('php://customers');
 
+// Converts it into a PHP object
+
+        $data = json_decode($json);
+
+        echo $data->name;
+        echo "\n";
+
+        echo $data->id;
         //you should not echo anything inside your controller - only assign vars here
         // then the view will actually display them.
 
