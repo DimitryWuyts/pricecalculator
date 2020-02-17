9e3d258bea681d4b75c10f61fa5d75ece6a9ce61
dimitry <dimiw1337@hotmail.com>
2020-02-17 04:00:48 -0500

    changed path for model and user

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 declare(strict_types=1);
 
 //include all your model files here
-require 'Model/User.php';
+require 'model/User.php';
 //include all your controllers here
-require 'Controller/HomepageController.php';
+require 'controller/HomepageController.php';
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
