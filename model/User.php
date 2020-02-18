f133abf701d3e07ad38a6512c896633d7ab55b93
dimitry <dimiw1337@hotmail.com>
2020-02-18 03:44:05 -0500

    fixing models, added php files to models

--- model/User.php
+++ model/User.php
@@ -4,15 +4,31 @@ declare(strict_types=1);
 
 class User
 {
-    private $name;
+    // private customer information
+    private $customerId;
+    private $customerGroupId;
+    private $customerName;
 
-    public function __construct(string $name)
+    // constructor (parent)
+    public function __construct(int $customerId, string $name, int $customerGroupId)
     {
-        $this->name = $name;
-    }
+        $this->customerId = $customerId;
+        $this->customerGroupId = $customerGroupId;
+        $this->customerName = $name;
 
+        //get
+    }
+    public function getId() : int
+    {
+        return $this->customerId;
+    }
+    public function getGroupId() : int
+    {
+        return $this->customerGroupId;
+    }
     public function getName() : string
     {
-        return $this->name;
+        return $this->customerName;
     }
+
 }
