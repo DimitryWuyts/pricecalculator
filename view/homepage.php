a49f2ddc8f880cc8e31c9e5ad9579e8a6d80f7d3
moeti <moetichentuf@hotmail.com>
2020-02-18 12:03:15 +0100

    added dropdown

--- view/homepage.php
+++ view/homepage.php
@@ -10,12 +10,26 @@
     <title>PriceCalculator</title>
 </head>
 <body>
-<?php require 'includes/header.php'?>
+
 <section>
     <h4>Hello </h4>
     <p>Put your content here.</p>
 </section>
-<?php require 'includes/footer.php'?>
+<label for="Products">Products::</label>
+
+<select id="Products">
+    <option value="Product"></option>
+    <option value="Customer"></option>
+
+</select>
+<label for="Customers">Customers:</label>
+
+<select id="Customers">
+    <option value="Product"></option>
+    <option value="Customer"></option>
+
+</select>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
